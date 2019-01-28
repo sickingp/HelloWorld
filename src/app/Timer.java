1a5204e36e71856053d36a7bc65d1a17ac863a2d
Paul Sicking <sickingp@gmail.com>
2019-01-27 22:16:07 -0800

    Edit msg in increment

--- src/app/Timer.java
+++ src/app/Timer.java
@@ -8,7 +8,7 @@ class Timer {
     }
 
     public double increment(double incr) {
-        String incrementing = "Incrementing";
+        String incrementing = "Incrementing in Timer class";
         mTime += incr;
         System.out.println(incrementing);
         return mTime;
